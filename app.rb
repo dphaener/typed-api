4567fc81cc804dac1e49b117347906305cf893c5
dphaener <dphaener@gmail.com>
2017-01-12 10:48:19 -0800

    Add graphiql documentation

--- app.rb
+++ app.rb
@@ -2,17 +2,29 @@ require "rubygems"
 require "bundler/setup"
 
 require "sinatra/base"
-require "dotenv"
 
-Dotenv.load
+require_relative "system/container"
+
+TypedAPI.boot!(:persistence)
+TypedAPI.boot!(:graph)
 
 class App < Sinatra::Base
-  enable :sessions, :protection
-  set :session_secret, ENV.fetch("SECRET")
-
   use Rack::Deflater
 
-  post "/graphql" do
+  get "/docs" do
+    send_file "./app/views/docs.html"
+  end
 
+  post "/graphql" do
+    payload = JSON.parse(request.body.read)
+    result = TypedAPI["graph.query"].(
+      schema: TypedAPI["graph.schema"],
+      query: payload["query"],
+      variables: payload["variables"],
+      context: {
+        db: TypedAPI["persistence.db"]
+      }
+    )
+    result.to_json
   end
 end
